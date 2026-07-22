ea072f12fc78e9f7420d047515a1aa11389f8436
Aymara1010 <aymaraandersen2007@gmail.com>
2026-07-21 20:12:05 -0400

    Reorganización de páginas (Cambio de Rutas)

--- app.py
+++ app.py
@@ -2,18 +2,24 @@ import streamlit as st
 
 st.set_page_config(page_title="Computación II", page_icon="💻", layout="wide")
 
-pg_00 = st.Page("capitulos/00_inicio.py", title="Inicio")
-pg_01 = st.Page("capitulos/01_Introduccion.py", title="Introducción")
-pg_02 = st.Page("capitulos/02_Normalizacion.py", title="Normalizacion")
-pg_03 = st.Page("capitulos/03_Denormalizacion.py", title="Denormalización")
-pg_04 = st.Page("capitulos/04_DER.py", title="Introducción a DER")
-pg_05 = st.Page("capitulos/05_SQL.py", title="Introducción a SQL")
+pg_00 = st.Page("Indice/GENERAL/00_inicio.py", title="Inicio")
+pg_01 = st.Page("Indice/GENERAL/01_Introduccion.py", title="Introducción")
+
+pg_02 = st.Page("Indice/TEMA 01/00_Normalizacion.py", title="Normalizacion")
+pg_03 = st.Page("Indice/TEMA 01/01_Denormalizacion.py", title="Denormalización")
+pg_04 = st.Page("Indice/TEMA 01/02_EjerciciosTema1.py", title="Ejecicios Resueltos")
+
+pg_05 = st.Page("Indice/TEMA 02/00_DER.py", title="Introducción a DER")
+pg_06 = st.Page("Indice/TEMA 02/01_NotacionDER.py", title="Fundamentos y Notación de DER")
+pg_07 = st.Page("Indice/TEMA 02/02_EjerciciosTema2.py", title="Ejercicios Resueltos")
+
+pg_08 = st.Page("Indice/TEMA 03/00_SQL.py", title="Introducción a SQL")
 
 pg = st.navigation({
     "**General**": [pg_00, pg_01],
-    "**Tema 01: Normalización y Denormalización**": [pg_02, pg_03],
-    "**Tema 02: Diagrama Entidad Relación**": [pg_04],
-    "**Tema 03: SQL**": [pg_05]
+    "**Tema 01: Normalización y Denormalización**": [pg_02, pg_03, pg_04],
+    "**Tema 02: Diagrama Entidad Relación**": [pg_05, pg_06, pg_07],
+    "**Tema 03: SQL**": [pg_08]
 })
 
 pg.run()
