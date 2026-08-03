b32b5a7600e7dc6576a794106b5116047e1a96c1
Aymara1010 <aymaraandersen2007@gmail.com>
2026-08-03 16:54:57 -0400

    commit de prueba

--- Indice/TEMA 01/00_Normalizacion.py
+++ Indice/TEMA 01/00_Normalizacion.py
@@ -2,6 +2,7 @@ import streamlit as st
 import utils
 
 st.title("Normalización")
+
 st.markdown("⚠️🔨 En Construcción... 🔨⚠️")
 
 utils.cambio_pag(ant="Indice/TEMA 00/02_TiposdeDatos.py", sig="Indice/TEMA 01/01_Denormalizacion.py")
