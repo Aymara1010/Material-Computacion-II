9c90d5801dcb36ae14c9bf2fc5cd694abcc068e0
Aymara1010 <aymaraandersen2007@gmail.com>
2026-08-04 16:29:47 -0400

    Se cambia el nombre de las páginas del Tema 01 (Cambio de Rutas)

--- Indice/TEMA 01/00_Normalizacion.py
+++ Indice/TEMA 01/00_Normalizacion.py
@@ -3,6 +3,25 @@ import utils
 
 st.title("Normalización")
 
-st.markdown("⚠️🔨 En Construcción... 🔨⚠️")
+st.markdown("""
+    <style>
+    .justificar {
+        text-align: justify;
+    }
+    .destacar {
+        color: #FF4B4B;
+        font-weight: bold;
+    }
+    .contenedor {
+        background-color: #1E2023;
+        padding: 15px;
+        border-radius: 5px;
+        border-left: 5px solid #FF4B4B;
+        margin-bottom: 10px;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
-utils.cambio_pag(ant="Indice/TEMA 00/02_TiposdeDatos.py", sig="Indice/TEMA 01/01_Denormalizacion.py")
+st.markdown("⚠️ En Construcción... ",unsafe_allow_html=True)
+
+utils.cambio_pag(ant="Indice/TEMA 00/02_TiposdeDatos.py", sig="Indice/TEMA 01/01_Arquitectura.py")
